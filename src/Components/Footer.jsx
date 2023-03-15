1ada199f7b2cee03196b5e6869183fc3b756441a
balasurya984 <balasurya6979@gmail.com>
2023-03-15 20:27:30 +0530

    location

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import styles from '../styles/Footer.module.scss'
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+import store from "./store";
+
 const Footer = () => {
   return (
-    <div className={styles.container}>Footer</div>
+    <div className='row' style={{display:"flex"}}>
+      <div  className={`col-sm-12 col-md-6 col-lg-4 my-1 shadow mx-3 ${styles.cards}`}>
+        <img src={store} alt="" /><br />
+        <h2>Location : Koduvai</h2>
+        <p className={styles.address} >
+        <h4>Address </h4>
+          <p>Rajalakshmi Agencies <br />Dharapuram Main Road,<br />Koduvai - 638660. </p>
+        </p>
+      </div>
+
+      <div className={`col-sm-12 col-md-6 col-lg-4 my-1 shadow mx-3 ${styles.cards}`}>
+        <img src={store} alt="" /><br />
+        <h2>Location : Perunthuluvu</h2>
+        <p className={styles.address} >
+        <h4>Address </h4>
+          <p>Rajalakshmi Agencies <br />Dharapuram Main Road,<br />Koduvai - 638660. </p>
+        </p>
+      </div>
+
+      <div className={`col-sm-12 col-md-6 col-lg-4 my-1 shadow mx-3 ${styles.cards}`}>
+        <img src={store} alt="" /><br />
+        <h2>Location : Kundadam</h2>
+        <p className={styles.address} >
+        <h4>Address </h4>
+          <p>Rajalakshmi Agencies <br />Dharapuram Main Road,<br />Koduvai - 638660. </p>
+        </p>
+      </div>
+    </div>
   )
 }
 
