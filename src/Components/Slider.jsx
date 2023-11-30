d3bc3a820a1033fad45a64468e22b802e5c9c07e
Bharath <95229845+BHARATH2503@users.noreply.github.com>
2023-11-30 23:27:25 +0530

    Slider image update

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -7,11 +7,11 @@ import Link from "next/link";
 const images = [
   { url: "https://images.unsplash.com/photo-1656918828509-e674e5e99422?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTJ8fGFncmljdWx0dXJlJTIwcGVzdGlzaWRlJTIwc2hvcHN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=1500&q=600" },
   { url: "https://images.unsplash.com/photo-1628352081506-83c43123ed6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjcyfHxhZ3JpY3VsdHVyZSUyMHBlc3Rpc2lkZSUyMHNob3BzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1500&q=600" },
-  { url: "https://images.unsplash.com/photo-1655130944329-b3a63166f6b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fHNwcmF5aW5nJTIwcGVzdGlzaWRlcyUyMGFncmljdWx0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1500&q=1500" },
-  { url: "https://images.unsplash.com/photo-1655130944281-072e0644db75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTV8fHNwcmF5JTIwcGVzdGlzaWRlcyUyMGluJTIwYWdyaWN1bHR1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=1000" },
-  { url: "https://images.unsplash.com/photo-1589876568181-a1508b8ef473?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=1000" },
-  { url: "https://images.unsplash.com/photo-1597916829826-02e5bb4a54e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTY1fHxzcHJheWluZyUyMHBlc3Rpc2lkZXMlMjBhZ3JpY3VsdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=1000" },
-  { url: "https://images.unsplash.com/photo-1515150144380-bca9f1650ed9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=800" },
+  { url: "https://st4.depositphotos.com/1194063/25290/i/450/depositphotos_252907744-stock-photo-farmer-planting-tomatoes-seedling-in.jpg" },
+  { url: "https://st4.depositphotos.com/1194063/25290/i/380/depositphotos_252907536-stock-photo-farmer-planting-seeds-in-soil.jpg" },
+  { url: "https://media.gettyimages.com/id/1095359686/photo/agriculture-drone-fly-to-sprayed-fertilizer-on-the-rice-fields.jpg?s=612x612&w=0&k=20&c=sa613TCSs6sXt2fdcrOnis5EuN5qwckYpGmQvsEXVoM=" },
+  { url: "https://media.istockphoto.com/id/606230424/photo/agriculture.jpg?s=612x612&w=0&k=20&c=byKybbMGCEAgdgfgyvUXC2ttl_8nbfc7JKpKFYg6rw4=" },
+  { url: "https://media.istockphoto.com/id/1198739624/photo/science-research-education-concept.jpg?s=612x612&w=0&k=20&c=6TM0RhAyw2ll6WLNjmW9324SNCkw3r5ORIMaLjop6mk=" },
 ];
 
 const Slider = () => {
